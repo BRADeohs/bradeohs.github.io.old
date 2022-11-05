8d3b49d2913d04877f3e56284c440fbf6fb94509
BRADeohs <91292351+BRADeohs@users.noreply.github.com>
2022-11-04 17:48:48 -0700

    Update plusv2.js
    added support for unstructured email via xpath

--- plusv2.js
+++ plusv2.js
@@ -22,10 +22,31 @@ for (let i = 2; i < 9; i++) {
   let x=getElementByXpath("/html/body/div/div/table[2]/tbody/tr[3]/td/table/tbody/tr/td/div[1]/font/text()[" + i + "]");
   //let s = "body > div > div > table.message > tbody > tr:nth-child(3) > td > table > tbody > tr > td > div:nth-child(1) > font > div > div > div:nth-child(4) > table > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(" + i + ") > td:nth-child(1) > strong";
   //let ti = document.querySelector(s)
-  dM(x);
+  iD(x);
 }
 var mi=getElementByXpath("/html/body/div/div/table[2]/tbody/tr[3]/td/table/tbody/tr/td/div[1]/font/text()[18]");
-eM(mi);
+pQ(mi);
+
+function iD(select) {
+  let tii = select.split(' ');
+  let tdi = new MyDate(tii[1]);
+  let nextYear = tdi.addDays(364);
+  let yyyy = nextYear.getFullYear();
+  let mm = nextYear.getMonth() + 1;
+  let dd = nextYear.getDate();
+  let fin = tii[0] + ' ' + mm + '/' + dd + '/' + yyyy + tii[2] + tii[3] + tii[4] + tii[5] + tii[6];
+  select.textcontent = fin;   
+}
+
+function pQ(select) {
+  let tii = select.split(' - ');
+  let tdi = new MyDate(tii[1]);
+  let nextYear = tdi.addDays(364);
+  let fDate = nextYear.toLocaleString('en-US', { dateStyle: 'long' });
+  let fin = tii[0] + ' - ' + fDate;
+  select.textcontent = fin;   
+}
+
 
 function dM(select) {
   let tii = select.innerHTML.split(' ');
