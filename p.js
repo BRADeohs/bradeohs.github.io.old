d1b23d2611d8a02d865bf5b9bdb49099a337bf2d
BRADeohs <91292351+BRADeohs@users.noreply.github.com>
2022-11-08 09:56:53 -0700

    Update p.js
    comment out nonsense

--- p.js
+++ p.js
@@ -5,7 +5,7 @@
   head.js("https://code.jquery.com/jquery.min.js", function() {
     class MyDate extends Date {
   addDays(days) {
-    let date = new MyDate(this.valueOf());
+    let MyDate = new MyDate(this.valueOf());
     date.setDate(date.getDate() + days);
     return date;
   }
@@ -17,9 +17,9 @@ function getElementByXpath(path) {
 
  for (let i = 2; i < 9; i++) {
   let s = "body > div > div > table.message > tbody > tr:nth-child(3) > td > table > tbody > tr > td > div:nth-child(1) > font > div > div > div:nth-child(4) > table > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(" + i + ") > td:nth-child(1) > strong";
-  s = "body > div > div > table.message > tbody > tr:nth-child(3) > td > table > tbody > tr > td > div:nth-child(1) > font > div > div > div:nth-child(4) > table > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(" + i + ") > td:nth-child(1) > strong";
+  //s = "body > div > div > table.message > tbody > tr:nth-child(3) > td > table > tbody > tr > td > div:nth-child(1) > font > div > div > div:nth-child(4) > table > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(" + i + ") > td:nth-child(1) > strong";
   let ti = document.querySelector(s)
-  ti = document.querySelector(s)
+  //ti = document.querySelector(s)
   if (ti != null) {
     dM(ti);
   } else {
@@ -29,7 +29,7 @@ function getElementByXpath(path) {
 }
 
 var qi = document.querySelector("body > div > div > table.message > tbody > tr:nth-child(3) > td > table > tbody > tr > td > div:nth-child(1) > font > div > div > div:nth-child(4) > table > tbody > tr > td > table:nth-child(1) > tbody > tr > td:nth-child(2) > strong");
-qi = document.querySelector("body > div > div > table.message > tbody > tr:nth-child(3) > td > table > tbody > tr > td > div:nth-child(1) > font > div > div > div:nth-child(4) > table > tbody > tr > td > table:nth-child(1) > tbody > tr > td:nth-child(2) > strong");
+//qi = document.querySelector("body > div > div > table.message > tbody > tr:nth-child(3) > td > table > tbody > tr > td > div:nth-child(1) > font > div > div > div:nth-child(4) > table > tbody > tr > td > table:nth-child(1) > tbody > tr > td:nth-child(2) > strong");
 if (qi != null) {
     eM(qi);
 } 
@@ -37,78 +37,78 @@ if (qi != null) {
 function bakup() {
   for (let i = 12; i < 19; i++) {
     let x=getElementByXpath("/html/body/div/div/table[2]/tbody/tr[3]/td/table/tbody/tr/td/div[1]/font/text()[" + i + "]");
-    x=getElementByXpath("/html/body/div/div/table[2]/tbody/tr[3]/td/table/tbody/tr/td/div[1]/font/text()[" + i + "]");
+    //x=getElementByXpath("/html/body/div/div/table[2]/tbody/tr[3]/td/table/tbody/tr/td/div[1]/font/text()[" + i + "]");
     //let s = "body > div > div > table.message > tbody > tr:nth-child(3) > td > table > tbody > tr > td > div:nth-child(1) > font > div > div > div:nth-child(4) > table > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(" + i + ") > td:nth-child(1) > strong";
      //let ti = document.querySelector(s)
     iD(x);
     }
     let mi = getElementByXpath("/html/body/div/div/table[2]/tbody/tr[3]/td/table/tbody/tr/td/div[1]/font/text()[10]");
-    mi = getElementByXpath("/html/body/div/div/table[2]/tbody/tr[3]/td/table/tbody/tr/td/div[1]/font/text()[10]");
+    //mi = getElementByXpath("/html/body/div/div/table[2]/tbody/tr[3]/td/table/tbody/tr/td/div[1]/font/text()[10]");
     pQ(mi);
 }
 
 function iD(select) {
   let tii = select.textContent;
-  tii = tii.split(' ');
+  //tii = tii.split(' ');
   let tdi = new MyDate(tii[1]);
-  tdi = new MyDate(tii[1]);
+  //tdi = new MyDate(tii[1]);
   let nextYear = tdi.addDays(364);
-  nextYear = tdi.addDays(364);
+  //nextYear = tdi.addDays(364);
   let yyyy = nextYear.getFullYear();
-  yyyy = nextYear.getFullYear();
+  //yyyy = nextYear.getFullYear();
   let mm = nextYear.getMonth() + 1;
-  mm = nextYear.getMonth() + 1;
+  //mm = nextYear.getMonth() + 1;
   let dd = nextYear.getDate();
-  dd = nextYear.getDate();
+  //dd = nextYear.getDate();
   let fin = tii[0] + ' ' + mm + '/' + dd + '/' + yyyy + tii[2] + tii[3] + tii[4] + tii[5] + tii[6];
-  fin = tii[0] + ' ' + mm + '/' + dd + '/' + yyyy + tii[2] + tii[3] + tii[4] + tii[5] + tii[6];
+  //fin = tii[0] + ' ' + mm + '/' + dd + '/' + yyyy + tii[2] + tii[3] + tii[4] + tii[5] + tii[6];
   select.textContent = fin;   
 }
 
 function pQ(select) {
   let tii = select.textContent;
-  tii = tii.split(' ');
+  //tii = tii.split(' ');
   let tdi = new MyDate(tii[1]);
-  tdi = new MyDate(tii[1]);
+  //tdi = new MyDate(tii[1]);
   let nextYear = tdi.addDays(364);
-  nextYear = tdi.addDays(364);
+  //nextYear = tdi.addDays(364);
   let fDate = nextYear.toLocaleString('en-US', { dateStyle: 'long' });
-  fDate = nextYear.toLocaleString('en-US', { dateStyle: 'long' });
+  //fDate = nextYear.toLocaleString('en-US', { dateStyle: 'long' });
   let fin = tii[0] + ' - ' + fDate;
-  fin = tii[0] + ' - ' + fDate;
+  //fin = tii[0] + ' - ' + fDate;
   select.textContent = fin;   
 }
 
 
 function dM(select) {
   let tii = select.innerHTML.split(' ');
-  tii = select.innerHTML.split(' ');
+  //tii = select.innerHTML.split(' ');
   let tdi = new MyDate(tii[1]);
-  tdi = new MyDate(tii[1]);
+  //tdi = new MyDate(tii[1]);
   let nextYear = tdi.addDays(364);
-  nextYear = tdi.addDays(364);
+  //nextYear = tdi.addDays(364);
   let yyyy = nextYear.getFullYear();
-  yyyy = nextYear.getFullYear();
+  //yyyy = nextYear.getFullYear();
   let mm = nextYear.getMonth() + 1;
-  mm = nextYear.getMonth() + 1;
+  //mm = nextYear.getMonth() + 1;
   let dd = nextYear.getDate();
-  dd = nextYear.getDate();
+  //dd = nextYear.getDate();
   let fin = tii[0] + ' ' + mm + '/' + dd + '/' + yyyy;
-  fin = tii[0] + ' ' + mm + '/' + dd + '/' + yyyy;
+  //fin = tii[0] + ' ' + mm + '/' + dd + '/' + yyyy;
   select.innerHTML = fin;   
 }
 
 function eM(select) {
   let tii = select.innerHTML.split(' - ');
-  tii = select.innerHTML.split(' - ');
+  //tii = select.innerHTML.split(' - ');
   let tdi = new MyDate(tii[1]);
-  tdi = new MyDate(tii[1]);
+  //tdi = new MyDate(tii[1]);
   let nextYear = tdi.addDays(364);
-  nextYear = tdi.addDays(364);
+  //nextYear = tdi.addDays(364);
   let fDate = nextYear.toLocaleString('en-US', { dateStyle: 'long' });
-  fDate = nextYear.toLocaleString('en-US', { dateStyle: 'long' });
+  //fDate = nextYear.toLocaleString('en-US', { dateStyle: 'long' });
   let fin = tii[0] + ' - ' + fDate;
-  fin = tii[0] + ' - ' + fDate;
+  //fin = tii[0] + ' - ' + fDate;
   select.innerHTML = fin;   
 }
   });
